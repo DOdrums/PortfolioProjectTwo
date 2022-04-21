285faab2a20c35d71d19492cd6c9b398ac4aecbc
DOdrums <dirkornee@hotmail.com>
2022-04-21 13:36:48 +0000

    add key up event to reset styling of key after press

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 document.addEventListener("keydown", function(event) {
     if (event.key === "f") {
-        document.getElementById("f-key").style.boxShadow = "inset 0px 0px 0px 6px grey";
+        document.getElementById("f-key").style.boxShadow = "inset 0px 0px 0px 6px rgb(233 233 233)";
+    } 
+})
+
+document.addEventListener("keyup", function(event) {
+    if (event.key === "f") {
+        document.getElementById("f-key").style.boxShadow = "rgb(2 166 255) 0px 0px 8px 0px";
     }
 })
