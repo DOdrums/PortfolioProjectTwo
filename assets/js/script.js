f60527ee9ca732c7e20de2c66e68059c3a9c2320
DOdrums <dirkornee@hotmail.com>
2022-04-21 15:14:50 +0000

    change key event listener to listen to any key and add nescesarry ids to all spans

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
+function getKeys(keys) {
+    let letters = []
+    for (let key in keys) {
+        letters.push(keys[key].innerText)
+    }
+    return letters
+}
+
+keys = document.getElementsByClassName('key')
+let letters = getKeys(keys)
+
+console.log(letters)
+
 document.addEventListener("keydown", function(event) {
-    if (event.key === "f") {
-        document.getElementById("f-key").style.boxShadow = "inset 0px 0px 0px 6px rgb(233 233 233)";
-    } 
+    if (event.key === event.key ) {
+        console.log(event.key)
+        document.getElementById(event.key + '-key').style.boxShadow = "inset 0px 0px 0px 6px rgb(233 233 233)";
+    }
 })
 
 document.addEventListener("keyup", function(event) {
-    if (event.key === "f") {
-        document.getElementById("f-key").style.boxShadow = "rgb(2 166 255) 0px 0px 8px 0px";
+    if (event.key === event.key) {
+        document.getElementById(event.key + '-key').style.boxShadow = "rgb(2 166 255) 0px 0px 8px 0px";
     }
 })
