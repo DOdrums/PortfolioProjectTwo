66a90dfcdc5d2039a7aac59fbff6c8b42d2af65f
DOdrums <dirkornee@hotmail.com>
2022-04-28 15:26:41 +0000

    add function to compare typed text to texed to be typed per letter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-typedLetters = []
-
 document.addEventListener("keydown", function(event) {
     if (event.key === event.key ) {
-        typedLetters.push(event.key)
+        typedLetters.push(event.key);
+        console.log(typedLetters);
+        compareText();
         document.getElementById(event.key + '-key').style.boxShadow = "inset 0px 0px 0px 6px rgb(233 233 233)";
-        printLetter()
     }
 })
 
@@ -16,9 +15,23 @@ document.addEventListener("keyup", function(event) {
 
 function printLetter() {
     for (let letter in typedLetters) {
-        console.log(typedLetters[letter])
+        console.log(typedLetters[letter]);
     }
 }
+
+
+function compareText() {
+    for (let letter = 0; letter < typeText.length; letter++) {
+        if (typedLetters[letter] == typeText[letter]) {
+            console.log("Good Job!");
+        } else {
+            console.log(typeText);
+        }
+    }    
+}
+
+typedLetters = [];
+typeText = ['Shift', 'T', 'y', 'p', 'e'];
 
 // nocode:
     `list gets appended with each keystroke, after which a function is called to compare said list to list of letters in text that has to be typed
