4772af7b91e5db42e7784d9ac0ba41e088288760
DOdrums <dirkornee@hotmail.com>
2022-04-29 14:39:25 +0000

    further tweak listeners so typed text is dealt with in the correct manner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 document.addEventListener("keydown", function(event) {
     if (event.key === event.key ) {
-        if (event.key !== "Shift" && event.key !== " ") {
-            document.getElementById((event.key).toLowerCase() + '-key').style.boxShadow = "inset 0px 0px 0px 6px rgb(233 233 233)";
+        if (event.key === "Shift") {
+            // do nothing
+        } else if (event.key === " ") {
+            typedLetters.push(event.key);
+            console.log(typedLetters);
+            compareText();
         } else {
-            // don't do anything
+            document.getElementById((event.key).toLowerCase() + '-key').style.boxShadow = "inset 0px 0px 0px 6px rgb(233 233 233)";
+            typedLetters.push(event.key);
+            console.log(typedLetters);
+            compareText();
         }
-        typedLetters.push(event.key);
-        console.log(typedLetters);
-        compareText();
     }
 })
 
@@ -18,17 +22,12 @@ document.addEventListener("keyup", function(event) {
     }
 })
 
-function printLetter() {
-    for (let letter in typedLetters) {
-        console.log(typedLetters[letter]);
-    }
-}
-
 
 function compareText() {
         if (typedLetters[typedLetters.length - 1] === typeText[typedLetters.length - 1]) {
             console.log("Good Job!");
-            text = [document.getElementById("text-box").textContent]
+            
+            text = document.getElementById("text-box").innerText.charAt(typedLetters.length - 1)
             console.log(text)
         } else {
             console.log("typeText");
@@ -36,12 +35,20 @@ function compareText() {
 }
 
 typedLetters = [];
-typeText = ['T', 'y', 'p', 'e'];
+typeText = ['T', 'y', 'p', 'e', ' ', 't', 'h', 'i', 's', ' ', 't', 'e', 'x', 't', ' ', 'a', 's', ' ', 'f', 'a', 's', 't', ' ', 'a', 's', ' ', 'y', 'o', 'u', ' ', 'c', 'a', 'n', '.'];
 
 // nocode:
-    `list gets appended with each keystroke, after which a function is called to compare said list to list of letters in text that has to be typed
+    // `list gets appended with each keystroke, after which a function is called to compare said list to list of letters in text that has to be typed
 
-    If backspace is hit, pop last item off of list, so text can be reasessed (option)
+    // If backspace is hit, pop last item off of list, so text can be reasessed (option)
 
-    When restart button is hit, text disappears and typedLetters list is reset.
-    `
+    // When restart button is hit, text disappears and typedLetters list is reset.
+    // `
+
+// Logic first listener:
+
+    // ` If shift do nothing
+    // If space or other key, call function
+    // If key but not space or shift, call color change
+
+    // `
